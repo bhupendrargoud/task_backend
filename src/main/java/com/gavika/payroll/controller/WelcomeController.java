c4670297f7da9e1ba4240933c6ba19c5881c7c14
gavika <bhupendrargoud@gmail.com>
2024-02-05 16:28:07 +0530

    cross updates

--- src/main/java/com/gavika/payroll/controller/WelcomeController.java
+++ src/main/java/com/gavika/payroll/controller/WelcomeController.java
@@ -2,15 +2,16 @@ package com.gavika.payroll.controller;
 
 
 
-	import org.springframework.web.bind.annotation.GetMapping;
+	import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 	import org.springframework.web.bind.annotation.RestController;
-
+	@CrossOrigin(origins = "*")
 	@RestController
 	public class WelcomeController {
 
 	    @GetMapping("/")
 	    public String welcome() {
-	        return "Hello from the backend of the Payroll application @gavika pipe line test";
+	        return " Payroll application @gavika ";
 	    }
 	}
 
